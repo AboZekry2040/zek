11587df5a5a823c6b09c6baa0cfad87823e46cd1
AboZekry <mohamedzekry2040@gmail.com>
2025-03-29 06:44:59 +0200

    Delete scripts.dae109b8edd9f943.js

--- scripts.dae109b8edd9f943.js
+++
@@ -1,1 +0,0 @@
-function angularJs(){let e=document.querySelector(".audio");document.addEventListener("click",function(o){e.play()},{once:!0})}function About(){let e=document.querySelector(".cancle"),o=document.querySelector(".video");e.onclick=function(){o.pause()},console.log("stop")}
